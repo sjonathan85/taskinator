626f1f5d9e520fb8c6c8b628cb76191ee00f7448
Sjonathan <sjonchristensen@gmail.com>
2021-11-18 20:16:50 -0700

    added event handler to the createTaskHandler function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,17 @@
-var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form");
 var TasksToDoEl = document.querySelector("#tasks-to-do");
 
 
 
 var createTaskHandler = function() {
+
+    event.preventDefault();
+
     var ListItemEl = document.createElement("li");
     ListItemEl.className = "task-item";
     ListItemEl.textContent= "This is a new task.";
     TasksToDoEl.appendChild(ListItemEl);
+
 };
 
-buttonEl.addEventListener("click", createTaskHandler);
+formEl.addEventListener("submit", createTaskHandler);
